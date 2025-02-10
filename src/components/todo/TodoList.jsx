49b9e6fefe7f0fc9d5eb032d11ca61f8267a4be2
hj <hyeonjubaek06@gmail.com>
2025-02-10 10:08:50 +0900

    TodoList 전체 레이어 별 styled

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,26 +1,45 @@
 // src/components/TodoList.jsx
 
+import styled from "styled-components";
 import TodoItem from "./TotoItem";
 
 const TodoList = ({ todos, handleToggleCompleted, handleDelete }) => {
     return (
         // return (소괄호) 안에는 하나의 태그만 들어갈 수 있어서 div로 감싸줌줌
-        <>
+        <TodoListSection>
+            <TodoListHeater>Tasks</TodoListHeater>
             {/* ul, li 태그로 시맨틱한 리스트 작성 */}
-            <ul>
+            <TodoListContent>
                 {todos.map(({ id, text, completed }) => (
-                    <TodoItem 
+                    <TodoItem
                         key={id}
                         completed={completed}
                         text={text}
                         handleToggleCompleted={handleToggleCompleted}
                         handleDelete={handleDelete}
-                        id={id} 
+                        id={id}
                     />
                 ))}
-            </ul>
-            </>
+            </TodoListContent>
+        </TodoListSection>
     )
-}
+};
+
+const TodoListSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
+
+const TodoListHeater = styled.h2`
+    font-size: 1.5rem;
+    font-weight: bold;
+`;
+
+const TodoListContent = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
 
 export default TodoList;
