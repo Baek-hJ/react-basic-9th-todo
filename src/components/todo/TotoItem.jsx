66b5bddab9a25995a69ebed3dc03f5d753239a93
hj <hyeonjubaek06@gmail.com>
2025-02-13 09:37:26 +0900

    style: todoitem 컨포넌트 styled-components 적용 및 레이아웃 개선

--- src/components/todo/TotoItem.jsx
+++ src/components/todo/TotoItem.jsx
@@ -1,25 +1,23 @@
 import styled from "styled-components";
 
 
-const TodoItem = ({
-    completed,
-    text,
-    handleToggleCompleted,
-    handleDelete,
-    id
+const TodoItem = ({completed, text, toggleCompleted, deleteTodo, id
 }) => {
-
     return (
-
         <TodoItemWrapper>
-            <TodoItemText $completed={completed}>
-                {text}
-            </TodoItemText>
-            <TodoItemSection>
-                <ActionButton onClick={() => handleToggleCompleted(id)} $bgcolor='#582BE6'>
-                    {completed ? "취소하기" : "완료하기"}</ActionButton>
+            <TodoItemText $completed={completed}>{text}</TodoItemText>
 
-                <ActionButton onClick={() => handleDelete(id)} $bgcolor='#ff4033'>삭제하기</ActionButton>
+            <TodoItemSection>
+                <ActionButton 
+                    onClick={() => toggleCompleted(id)} 
+                    $bgcolor={completed ? '#242424' : '#582BE6'}
+                >
+                    {completed ? "취소하기" : "완료하기"}
+                </ActionButton>
+
+                <ActionButton onClick={() => deleteTodo(id)} $bgcolor='#ff4033'>
+                    삭제하기
+                </ActionButton>
             </TodoItemSection>
         </TodoItemWrapper>
     )
@@ -33,10 +31,13 @@ const TodoItemWrapper = styled.li`
     border-radius: 1rem;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
     justify-content: space-between;
+    align-items: center;
 `;
 
 const TodoItemText = styled.p`
-    text-decoration: ${({ completed }) => (completed ? "line-through" : "none")};
+    text-decoration: ${({ completed }) => 
+    (completed ? "line-through" : "none")};
+    color: ${({ $completed}) => ($completed ? '#a0a0a0' : 'black')};
 `;
 
 const TodoItemSection = styled.div`
