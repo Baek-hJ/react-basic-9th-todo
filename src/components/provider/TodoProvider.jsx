9eb24f3ba6b1cf4bfe03f0dacba3acfcdc2880f0
hj <hyeonjubaek06@gmail.com>
2025-02-21 10:49:19 +0900

    feat: TodoProvider에서 API 호출을 통해 할 일 목록 가져오기 및 수정 기능 추가

--- src/components/provider/TodoProvider.jsx
+++ src/components/provider/TodoProvider.jsx
@@ -1,31 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoContext } from "../../context/TodoContext";
-import { SAMPLE_TODOS } from "../../constants/sample-todos";
+import { todoClient } from "../../lib/todoClient";
 
 const TodoProvider = ({ children }) => {
-  const [todos, setTodos] = useState(SAMPLE_TODOS);
+  const [todos, setTodos] = useState();
 
-  const addTodos = (text) => {
-    setTodos([{ id: crypto.randomUUID(), text, completed: false }, ...todos]);
+  const getTodos = async () => {
+    const { data } = await todoClient.get("/");
+    setTodos(data);
+  }
+
+  const addTodos = async (text) => {
+    const { data } = await todoClient.get("/", {
+      text,
+      completed: false,
+    });
+    await getTodos();
+
+    return data;
   };
 
-  const toggleTodoCompleted = (id) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id
-          ? {
-              ...todo,
-              completed: !todo.completed,
-            }
-          : todo
-      )
-    );
-  };
+  const toggleTodoCompleted = async (id, currentCompleted) => {
+    const {data} = await todoClient.patch(`/${id}`, {
+      completed: !currentCompleted,
+  });
 
-  const deleteTodo = (id) => {
-    // todo.id가 내가 찾는 id와 같지 않을 때 true를 반환하여 그대로 남겨둠
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-  };
+  await getTodos();
+
+return data;
+};
+
+const deleteTodo = async (id) => {
+  const { data } = await todoClient.delete(`/${id}`);
+
+  await getTodos();
+
+  return data;
+}
 
   const getFilteredTodos = (selectedFilter) => {
     if (selectedFilter === "completed") {
@@ -38,6 +49,11 @@ const TodoProvider = ({ children }) => {
 
     return todos;
   };
+
+  // 전체가 아닌 소량의 데이터만 가져올때 map으로
+  useEffect (() => {
+    getTodos();
+  },[]);
 
   return (
     <TodoContext.Provider
