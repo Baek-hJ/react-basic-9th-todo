cee9a6318584d5669eeb0b6223e9127f485ab853
hj <hyeonjubaek06@gmail.com>
2025-03-14 01:30:02 +0900

    feat: 할 일 추가 폼에서 항목 추가 기능 구현

--- components/todo/TodoForm.tsx
+++ components/todo/TodoForm.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+"use client"
+
+import { createTodo } from '@/api/todo-api';
+import React, { FormEvent } from 'react'
 
 const TodoForm = () => {
+  const onSubmit = async (e: FormEvent<HTMLFormElement> ) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const todoText = FormData.get("todo-text") as string;
+  
+    await createTodo(todoText);
+
+    form.reset();
+  };
+
   return (
-    <div>TodoForm</div>
+    <form onSubmit={onSubmit}>
+      <input type="text" name='todo-text' required/>
+      <button type='submit'>추가하기</button>
+    </form>
   )
 }
 
