f70387f93cc6b173da16dc54884d3222cf852a72
hj <hyeonjubaek06@gmail.com>
2025-03-13 15:50:28 +0900

    feat: 전체 파일 삭제 및 프로젝트 초기화

--- api/todo-api.ts
+++
@@ -1,8 +0,0 @@
-import { Todo } from "@/types/todo.type";
-
-export const getTodos = async () => {
-    const response = await fetch("http://localhost:3000/todos");
-    const data: Todo[] = await response.json();
-
-    return data;
-};
--- backups/src/lib/todoClient.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const todoClient = axios.create({
-    baseURL: 'http://localhost:3000/todos',
-    timeout: 1000,
-})
